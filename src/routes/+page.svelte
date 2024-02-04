b986cc8da59cf8569a6b55288d64268078a46700
ecruz22004 <ecruz22004@gmail.com>
2024-02-03 21:42:15 -0800

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -23,9 +23,9 @@
 	</div>
 	<div class="w-full inline-flex flex-nowrap overflow-hidden [mask-image:_linear-gradient(to_right,transparent_0,_black_128px,_black_calc(100%-200px),transparent_100%)]">
 
-		<ul class="flex items-center -scale-x-[100%] justify-center md:justify-start [&_li]:mx-8 [&_img]:max-w-none animate-infinite-scroll" aria-hidden="true">
+		<ul class="flex items-center  justify-center md:justify-start [&_li]:mx-8 [&_img]:max-w-none animate-infinite-scroll" aria-hidden="true">
 			<li>
-				<img src="/images/ahf1.jpg" alt="Facebook" />
+				<image href="/images/ahf1.jpg" />
 			</li>
 			<li>
 				<img src="/images/ahf2.jpg" alt="Disney" />
