d848f3a28994d25af20b5749dcbbee294e2c18a0
ecruz22004 <ecruz22004@gmail.com>
2024-02-03 22:13:21 -0800

    Transferred Files
    So i transferred the images to the lib file and imported the images into the main page. Gonna see if this works lol

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,28 @@
 <!-- YOU CAN DELETE EVERYTHING IN THIS PAGE -->
+<script>
+	import img1 from '$lib/images/ahf1.jpg'
+	import img2 from '$lib/images/ahf2.jpg'
+	import img3 from '$lib/images/ahf3.jpg'
+	import img4 from '$lib/images/ahf4.jpg'
+	import img5 from '$lib/images/ahf5.jpg'
+	import img6 from '$lib/images/ahf6.jpg'
+	import img7 from '$lib/images/ahf7.jpg'
+	import img8 from '$lib/images/ahf8.jpg'
+	import img9 from '$lib/images/ahf9.jpg'
+	import img10 from '$lib/images/ahf10.jpg'
+	import img11 from '$lib/images/ahf11.jpg'
+	import img12 from '$lib/images/ahf12.jpg'
+	import img13 from '$lib/images/ahf13.jpg'
+	import img14 from '$lib/images/ahf14.jpg'
+	import img15 from '$lib/images/ahf15.jpg'
+	import img16 from '$lib/images/ahf16.jpg'
+	import img17 from '$lib/images/ahf17.jpg'
+	import img18 from '$lib/images/ahf18.jpg'
+	import img19 from '$lib/images/ahf19.jpg'
+	import img20 from '$lib/images/ahf20.jpg'
+	import img21 from '$lib/images/ahf21.jpg'
+	import img22 from '$lib/images/ahf22.jpg'
+</script>
 
 <div class="container h-full mx-auto flex justify-center items-center">
 	<div class="space-y-10 text-center flex flex-col items-center">
@@ -25,70 +49,70 @@
 
 		<ul class="flex items-center  justify-center md:justify-start [&_li]:mx-8 [&_img]:max-w-none animate-infinite-scroll" aria-hidden="true">
 			<li>
-				<img src="/images/ahf1.jpg" alt="Facebook" />
+				<img src="{img1}" alt="Facebook" />
 			</li>
 			<li>
-				<img src="/images/ahf2.jpg" alt="Disney" />
+				<img src="{img2}" alt="Disney" />
 			</li>
 			<li>
-				<img src="/images/ahf3.jpg" alt="Airbnb" />
+				<img src="{img3}" alt="Airbnb" />
 			</li>
 			<li>
-				<img src="/images/ahf4.jpg" alt="Apple" />
+				<img src="{img4}" alt="Airbnb" />
 			</li>
 			<li>
-				<img src="/images/ahf5.jpg" alt="Apple" />
+				<img src="{img5}" alt="Airbnb" />
 			</li>
 			<li>
-				<img src="/images/ahf6.jpg" alt="Spark" />
+				<img src="{img6}" alt="Airbnb" />
 			</li>
 			<li>
-				<img src="/images/ahf7.jpg" alt="Samsung" />
+				<img src="{img7}" alt="Airbnb" />
 			</li>
 			<li>
-				<img src="/images/ahf8.jpg" alt="Quora" />
+				<img src="{img8}" alt="Airbnb" />
 			</li>
 			<li>
-				<img src="/images/ahf9.jpg" alt="Sass" />
+				<img src="{img9}" alt="Airbnb" />
 			</li>
 			<li>
-				<img src="/images/ahf10.jpg" alt="Facebook" />
+				<img src="{img10}" alt="Airbnb" />
 			</li>
 			<li>
-				<img src="/images/ahf11.jpg" alt="Facebook" />
+				<img src="{img11}" alt="Airbnb" />
 			</li>
 			<li>
-				<img src="/images/ahf12.jpg" alt="Disney" />
+				<img src="{img12}" alt="Airbnb" />
 			</li>
 			<li>
-				<img src="/images/ahf13.jpg" alt="Airbnb" />
+				<img src="{img13}" alt="Airbnb" />
 			</li>
 			<li>
-				<img src="/images/ahf14.jpg" alt="Apple" />
+				<img src="{img14}" alt="Airbnb" />
 			</li>
 			<li>
-				<img src="/images/ahf15.jpg" alt="Spark" />
+				<img src="{img15}" alt="Airbnb" />
 			</li>
 			<li>
-				<img src="/images/ahf16.jpg" alt="Samsung" />
+				<img src="{img16}" alt="Airbnb" />
 			</li>
 			<li>
-				<img src="/images/ahf17.jpg" alt="Quora" />
+				<img src="{img17}" alt="Airbnb" />
 			</li>
 			<li>
-				<img src="/images/ahf18.jpg" alt="Sass" />
+				<img src="{img18}" alt="Airbnb" />
 			</li>
 			<li>
-				<img src="/images/ahf19.jpg" alt="Facebook" />
+				<img src="{img19}" alt="Airbnb" />
 			</li>
 			<li>
-				<img src="/images/ahf20.jpg" alt="Facebook" />
+				<img src="{img20}" alt="Airbnb" />
 			</li>
 			<li>
-				<img src="/images/ahf21.jpg" alt="Disney" />
+				<img src="{img21}" alt="Airbnb" />
 			</li>
 			<li>
-				<img src="/images/ahf22.jpg" alt="Airbnb" />
+				<img src="{img22}" alt="Airbnb" />
 			</li>
 		</ul>            
 	</div>
