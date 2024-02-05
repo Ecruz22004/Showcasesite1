d7d1c54691026484711602518d28420186a1c156
ecruz22004 <ecruz22004@gmail.com>
2024-02-05 02:10:34 -0800

    THIS WORKS?!
    Lol maybe lets see

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,26 @@
 <script>
-    const images = import.meta.glob('./*.jpg');
-    const imageUrls = Object.keys(images);
+	import img1 from '$lib/images/ahf1.jpg'
+	import img2 from '$lib/images/ahf2.jpg'
+	import img3 from '$lib/images/ahf3.jpg'
+	import img4 from '$lib/images/ahf4.jpg'
+	import img5 from '$lib/images/ahf5.jpg'
+	import img6 from '$lib/images/ahf6.jpg'
+	import img7 from '$lib/images/ahf7.jpg'
+	import img8 from '$lib/images/ahf8.jpg'
+	import img9 from '$lib/images/ahf9.jpg'
+	import img10 from '$lib/images/ahf10.jpg'
+	import img11 from '$lib/images/ahf11.jpg'
+	import img12 from '$lib/images/ahf12.jpg'
+	import img13 from '$lib/images/ahf13.jpg'
+	import img14 from '$lib/images/ahf14.jpg'
+	import img15 from '$lib/images/ahf15.jpg'
+	import img16 from '$lib/images/ahf16.jpg'
+	import img17 from '$lib/images/ahf17.jpg'
+	import img18 from '$lib/images/ahf18.jpg'
+	import img19 from '$lib/images/ahf19.jpg'
+	import img20 from '$lib/images/ahf20.jpg'
+	import img21 from '$lib/images/ahf21.jpg'
+	import img22 from '$lib/images/ahf22.jpg'
 </script>
 
 <div class="container h-full mx-auto flex justify-center items-center">
@@ -29,11 +49,72 @@
 		    <div class="w-full inline-flex flex-nowrap overflow-hidden [mask-image:_linear-gradient(to_right,transparent_0,_black_128px,_black_calc(100%-200px),transparent_100%)]">
 
 		    	<ul class="flex items-center justify-center md:justify-start animate-infinite-scroll" aria-hidden="true">
-					{#each imageUrls as img}
 					<li class="mx-8">
-						<img class="max-w-none" src={img} alt="Celeb" />
+						<img class="max-w-none" src={img1} alt="Celeb" />
 					</li>
-					{/each}
+					<li class="mx-8">
+						<img class="max-w-none" src={img2} alt="Celeb" />
+					</li>
+					<li class="mx-8">
+						<img class="max-w-none" src={img3} alt="Celeb" />
+					</li>
+					<li class="mx-8">
+						<img class="max-w-none" src={img4} alt="Celeb" />
+					</li>
+					<li class="mx-8">
+						<img class="max-w-none" src={img5} alt="Celeb" />
+					</li>
+					<li class="mx-8">
+						<img class="max-w-none" src={img6} alt="Celeb" />
+					</li>
+					<li class="mx-8">
+						<img class="max-w-none" src={img7} alt="Celeb" />
+					</li>
+					<li class="mx-8">
+						<img class="max-w-none" src={img8} alt="Celeb" />
+					</li>
+					<li class="mx-8">
+						<img class="max-w-none" src={img9} alt="Celeb" />
+					</li>
+					<li class="mx-8">
+						<img class="max-w-none" src={img10} alt="Celeb" />
+					</li>
+					<li class="mx-8">
+						<img class="max-w-none" src={img11} alt="Celeb" />
+					</li>
+					<li class="mx-8">
+						<img class="max-w-none" src={img12} alt="Celeb" />
+					</li>
+					<li class="mx-8">
+						<img class="max-w-none" src={img13} alt="Celeb" />
+					</li>
+					<li class="mx-8">
+						<img class="max-w-none" src={img14} alt="Celeb" />
+					</li>
+					<li class="mx-8">
+						<img class="max-w-none" src={img15} alt="Celeb" />
+					</li>
+					<li class="mx-8">
+						<img class="max-w-none" src={img16} alt="Celeb" />
+					</li>
+					<li class="mx-8">
+						<img class="max-w-none" src={img17} alt="Celeb" />
+					</li>
+					<li class="mx-8">
+						<img class="max-w-none" src={img18} alt="Celeb" />
+					</li>
+					<li class="mx-8">
+						<img class="max-w-none" src={img19} alt="Celeb" />
+					</li>
+					<li class="mx-8">
+						<img class="max-w-none" src={img20} alt="Celeb" />
+					</li>
+					<li class="mx-8">
+						<img class="max-w-none" src={img21} alt="Celeb" />
+					</li>
+					<li class="mx-8">
+						<img class="max-w-none" src={img22} alt="Celeb" />
+					</li>
 				</ul>          
 			</div>
 		</div>
